61a5b7a1ba5706cd239d5fd33b4dadfc7ad768ca
koybasimuhittin <koybasimuhittin@gmail.com>
2023-10-27 17:07:34 +0300

    add website

---
+++ website/astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "astro/config"
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind"
+
+// https://astro.build/config
+import alpinejs from "@astrojs/alpinejs"
+
+// https://astro.build/config
+import image from "@astrojs/image"
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx"
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [tailwind(), alpinejs(), image(), mdx()],
+})
